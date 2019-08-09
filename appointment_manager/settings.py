2e0f0aa0c41e2079d5a3693533363b01aacc5f0b
Gustavo Francelino <gustavo.francelino@gmail.com>
2019-08-09 02:30:13 +0100

    add static root

--- appointment_manager/settings.py
+++ appointment_manager/settings.py
@@ -164,5 +164,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
