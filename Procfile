0cf6831f73cd6fd8759c313a650073058664eed0
GFrancelino <gustavo.francelino@gmail.com>
2020-12-01 17:37:16 +0000

    remove celery

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
 release: python manage.py migrate
 web: gunicorn appointment_manager.wsgi --log-file -
-worker: celery -A appointment_manager worker -l INFO
-beatworker: celery -A appointment_manager beat -l INFO
--- customers/tasks.py
+++ customers/tasks.py
@@ -1,7 +1,7 @@
-from celery import shared_task
-from appointment_manager.common.mail import send_mail
-
-
-@shared_task
-def send_test_mail():
-    send_mail('celery mail', 'did this work', ['gustavo.francelino@gmail.com'], True)
+# from celery import shared_task
+# from appointment_manager.common.mail import send_mail
+#
+#
+# @shared_task
+# def send_test_mail():
+#     send_mail('celery mail', 'did this work', ['gustavo.francelino@gmail.com'], True)
