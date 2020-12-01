eae2fde31c318221964bde888777beeae05da06d
GFrancelino <gustavo.francelino@gmail.com>
2020-12-01 18:38:31 +0000

    add celery

--- customers/tasks.py
+++
@@ -1,7 +0,0 @@
-# from celery import shared_task
-# from appointment_manager.common.mail import send_mail
-#
-#
-# @shared_task
-# def send_test_mail():
-#     send_mail('celery mail', 'did this work', ['gustavo.francelino@gmail.com'], True)
