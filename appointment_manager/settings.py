2ec1c7c4a17d5f0e76532caafa7445be2bca40bf
GFrancelino <gustavo.francelino@gmail.com>
2020-12-01 18:31:55 +0000

    fix bug added recently in settings

--- appointment_manager/settings.py
+++ appointment_manager/settings.py
@@ -126,7 +126,7 @@ ROOT_URLCONF = 'appointment_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -222,15 +222,15 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../../staticfiles')
-STATIC_TMP = os.path.join(BASE_DIR, '../../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../../static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Activate Django-Heroku.
