aaf533e3f0329a4298878957fb8f5ad8c1fbe668
GFrancelino <gustavo.francelino@gmail.com>
2021-01-12 20:26:52 +0000

    fix typo in url

--- billing/urls.py
+++ billing/urls.py
@@ -7,7 +7,7 @@ router.register(r'accounts', views.AccountViewSet, 'billing-account')
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path(r'connected-acccount-hooks/', views.ConnectedAccountHookView.as_view(), name='connected-webhook'),
+    path(r'connected-account-hooks/', views.ConnectedAccountHookView.as_view(), name='connected-webhook'),
     path(r'account-hooks/', views.AccountHookView.as_view(), name='account-webhook'),
     path(r'payment/', views.payment_intent, name='stripe-payment'),
 ]
