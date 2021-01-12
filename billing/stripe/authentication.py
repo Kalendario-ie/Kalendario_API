6a91dc960e404cdd896d3958572f0b910d8b1ff5
GFrancelino <gustavo.francelino@gmail.com>
2021-01-12 20:42:33 +0000

    update authentication to use stream body

--- billing/stripe/authentication.py
+++ billing/stripe/authentication.py
@@ -9,7 +9,7 @@ class StripeAuthentication(BasePermission):
     def has_permission(self, request, view):
         sig_header = request.META['HTTP_STRIPE_SIGNATURE']
         try:
-            view.kwargs['event'] = helpers.construct_event(request.data, sig_header, view.hook_secret)
+            view.kwargs['event'] = helpers.construct_event(request.stream.body, sig_header, view.hook_secret)
             return True
         except ValueError as e:
             return False
