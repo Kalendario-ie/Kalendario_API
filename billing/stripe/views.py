e0af7c8f130c0620b37d5ed4f3a6fbc7480ace8f
GFrancelino <gustavo.francelino@gmail.com>
2021-01-13 16:31:50 +0000

    use basic auth only for stripe hook

--- billing/stripe/views.py
+++ billing/stripe/views.py
@@ -1,28 +1,22 @@
 from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from . import helpers
+from billing.stripe.authentication import StripeAuthentication
 
 
 class StripeHookView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (StripeAuthentication, )
     hook_secret = None
     hook_handler_class = None
 
-    def get_event(self):
-        sig_header = self.request.META['HTTP_STRIPE_SIGNATURE']
-        return helpers.construct_event(self.request.stream.body, sig_header, self.hook_secret)
-
-    def post(self, request, *args, **kwargs):
+    def post(self, request, event, *args, **kwargs):
         """
         Returns 200 if everything went ok
         422 if the event type has no handler
         404 if the event doesn't belong to a know entity
         """
-        try:
-            event = self.get_event()
-        except ValueError as e:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-
         handler = self.hook_handler_class(event)
 
         if handler.invalid_event:
