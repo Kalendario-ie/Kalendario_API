03351879b2d7dc0c01c0614fca14c112d0aa2231
GFrancelino <gustavo.francelino@gmail.com>
2021-01-13 16:28:22 +0000

    update stripe view to use permission classess

--- billing/stripe/views.py
+++ billing/stripe/views.py
@@ -1,20 +1,28 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from billing.stripe.authentication import StripeAuthentication
+from . import helpers
 
 
 class StripeHookView(APIView):
-    permission_classes = [StripeAuthentication]
     hook_secret = None
     hook_handler_class = None
 
-    def post(self, request, event, *args, **kwargs):
+    def get_event(self):
+        sig_header = self.request.META['HTTP_STRIPE_SIGNATURE']
+        return helpers.construct_event(self.request.stream.body, sig_header, self.hook_secret)
+
+    def post(self, request, *args, **kwargs):
         """
         Returns 200 if everything went ok
         422 if the event type has no handler
         404 if the event doesn't belong to a know entity
         """
+        try:
+            event = self.get_event()
+        except ValueError as e:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
         handler = self.hook_handler_class(event)
 
         if handler.invalid_event:
